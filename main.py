7082b40caeaae7f363e9ce80a883d38e26e7fb9f
Lasie Hlongwane <lasielazba1@gmail.com>
2023-10-04 15:56:07 +0200

    easy commit

---
+++ main.py
@@ -0,0 +1,56 @@
+#Firstly, create a variable named story, where will have our missing words for the user
+#MAD LIB 1.
+
+# story = """
+# Roses are {}
+# violets are {}
+# Sugar is {}
+# And so are you!
+# """
+# #Ask the user to provide the missing words
+#
+# colour = input("Enter a colour: ")
+# colour2 = input("Enter another colour: ")
+# adjective = input("Enter an adjective: ")
+#
+# #Display the result
+# print(story.format(colour, colour2, adjective))
+
+#####################################################################
+
+#prompt the user to provide the missing words
+#MAD LIB 2.
+
+# noun1 = input("Enter your name: ")
+# noun2 = input("Enter your friends name: ")
+# noun3 = input("Enter another friends name: ")
+# place = input("Enter a place: ")
+# adjective1 = input("Enter an adjective: ")
+# adjective2 = input("Enter another adjective: ")
+# adjective3 = input("Enter one more adjective: ")
+# adverb = input("Enter an adverb: ")
+# adverb2 = input("Enter another adverb: ")
+# exclamation = input("Enter an emotion: ")
+#
+# # create a variable named story, where will add our missing words from the user
+# story = """
+# One day {noun1} went to {place}.
+# He felt very lonely, even though the view was {adjective1}.
+# He decided to call his two best friends {noun2} and {noun3}.
+# when they arrived they told {noun1} something {adjective2} had happened.
+# {noun1} went there very {adverb}
+# When he came he found out that his other friend was falling off a cliff.
+# {noun1} said {exclamation} inside he was feeling {adjective3}
+# {noun1} managed to save him
+# After that they had a {adverb2} celebration
+# They all laughed!
+# """
+#
+# #Display the result
+# print(story.format(noun1=noun1, noun2=noun2, noun3=noun3,
+#                    place=place,
+#                    adjective1=adjective1, adjective2=adjective2, adjective3=adjective3,
+#                    adverb=adverb, adverb2=adverb2,
+#                    exclamation=exclamation))
+
+
